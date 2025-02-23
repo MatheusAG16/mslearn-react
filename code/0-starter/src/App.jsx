8897148c19d30b21e4a6aa64cce720e2da44fed9
MatheusAG16 <mateusandradegermano@gmail.com>
2025-02-22 23:19:10 -0300

    Start React Microsoft-Learn

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RecipleTitle from "./RecipeTitle";
+import "./index.css";
+
+function App() {
+  return (
+    <article>
+      <h1>Recipe Manager</h1>
+      <RecipleTitle />
+    </article>
+  );
+}
+
+export default App;
---
+++ code/0-starter/src/RecipeTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function RecipleTitle() {
+  const title = "Mashed potatoes";
+  return <h2>{title}</h2>;
+}
+
+export default RecipleTitle;
