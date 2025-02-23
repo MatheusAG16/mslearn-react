ad2e221bfbe9982758d50d8929c5930a240e9efe
MatheusAG16 <mateusandradegermano@gmail.com>
2025-02-23 00:23:02 -0300

    Props em React - Microsoft Learn aula 2

---
+++ code/2-component-data/start/src/RecipeSteps.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function RecipeSteps() {
+  return (
+    <ol>
+      <li>Adicione as batatas cortadas a uma panela com água e sal.</li>
+      <li>Leve a panela para ferver.</li>
+      <li>
+        Ferva as batatas até ficarem macias, por cerca de 15 a 20 minutos.
+      </li>
+      <li>Coe as batatas.</li>
+      <li>Coloque-as novamente na panela.</li>
+      <li>Adicione a manteiga, o creme de leite, o sal e a pimenta a gosto.</li>
+      <li>Amasse as batatas</li>
+      <li>
+        Tempere novamente e adicione a manteiga e o creme de leite conforme
+        desejado.
+      </li>
+    </ol>
+  );
+}
+
+export default RecipeSteps;
